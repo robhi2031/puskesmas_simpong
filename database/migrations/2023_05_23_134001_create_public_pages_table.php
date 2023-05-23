00b6786a507c62d996c1c9001dca0749bee43b84
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-23 13:18:31 +0800

    Create 2023_05_23_134001_create_public_pages_table.php

---
+++ database/migrations/2023_05_23_134001_create_public_pages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublicPagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('public_pages', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('content')->nullable();
+            $table->text('slug')->nullable();
+            $table->string('is_trash', 1)->default('N');
+            $table->integer('user_add');
+            $table->integer('user_updated')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('NULL on update CURRENT_TIMESTAMP'))->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('public_pages');
+    }
+}
